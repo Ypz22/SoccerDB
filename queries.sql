d9fae5243be9296445643aaf3f82cce65b073ebd
Eduardo David García Romero <eduagarcia985@gmail.com>
2025-11-11 12:05:20 -0500

    Push 0.1
    Tabla jugadores creada y datos insertados

--- queries.sql
+++ queries.sql
@@ -6,9 +6,35 @@ CREATE TABLE teams(
         stadium VARCHAR(100), year_foundation INT
     );
 
+CREATE TABLE jugadores (
+    id SERIAL PRIMARY KEY,
+    nombre VARCHAR(50),
+    apellido VARCHAR(50),
+    edad INT,
+    altura DECIMAL(4,2),
+    pierna_buena VARCHAR(10),
+    club VARCHAR(100)
+);
+
+    INSERT INTO jugadores (nombre, apellido, edad, altura, pierna_buena, club) VALUES
+    ('Damián', 'Díaz', 37, 1.67, 'Derecha', 'Barcelona SC'),
+    ('Gabriel', 'Cortéz', 28, 1.78, 'Derecha', 'Barcelona SC'),
+
+    ('Miller', 'Bolaños', 34, 1.70, 'Derecha', 'Emelec'),
+    ('Pedro', 'Quiñónez', 38, 1.75, 'Derecha', 'Emelec'),
+
+    ('Alexander', 'Domínguez', 37, 1.95, 'Derecha', 'Liga Deportiva Universitaria de Quito'),
+    ('Edison', 'Vega', 34, 1.78, 'Izquierda', 'Liga Deportiva Universitaria de Quito'),
+
+    ('Junior', 'Sornoza', 30, 1.70, 'Derecha', 'Independiente del Valle'),
+    ('Lautaro', 'Díaz', 26, 1.84, 'Derecha', 'Independiente del Valle'),
+
+    ('Luis', 'Saritama', 40, 1.78, 'Derecha', 'Deportivo Quito'),
+    ('Franklin', 'Salas', 43, 1.68, 'Derecha', 'El Nacional');
+
 
     INSERT INTO teams(name, city, stadium, year_foundation)
-VALUES
+    VALUES
     ('Barcelona SC', 'Guayaquil', 'Estadio Monumental Banco Pichincha', 1925),
     ('Emelec', 'Guayaquil', 'Estadio George Capwell', 1929),
     ('Liga Deportiva Universitaria de Quito', 'Quito', 'Estadio Rodrigo Paz Delgado', 1930),
