1f55b21e44f48ccb53f94ab07461d9aeb8f8c78b
Jefferson Yepez <jeffersonyepez621@gmail.com>
2025-11-11 12:52:17 -0500

    changes

--- src/app.js
+++ src/app.js
@@ -1,17 +1,22 @@
-import express from 'express';
-import teamsRoutes from './routes/Teams.routes.js';
-import playerRoutes from './routes/Players.routes.js';
-import director from './routes/Director.routes.js';
-import ConnectDB from './config/db.js';
-import dotenv from 'dotenv';
+const express = require('express');
+const teamsRoutes = require('./routes/Teams.routes.js');
+const playerRoutes = require('./routes/Players.routes.js');
+const director = require('./routes/Director.routes.js');
+const ConnectDB = require('./config/db.js');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
 ConnectDB.connect();
+
 const PORT = process.env.PORT || 3000;
+
 app.use(express.json());
 app.use('/api/teams', teamsRoutes);
 app.use('/api/players', playerRoutes);
 app.use('/api/directors', director);
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
