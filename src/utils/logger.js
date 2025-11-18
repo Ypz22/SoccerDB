0328057ad447e54de8b27ab49a5b3db9ca83a550
Eduardo David García Romero <eduagarcia985@gmail.com>
2025-11-18 08:15:22 -0500

    actualizacion 1.6
    looger arreglado

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,8 +1,9 @@
-/* eslint-env node */
+/* eslint-disable no-console */
+/* global console */
 
 const logger = {
     info: (...msg) => console.log(...msg),
-    error: (...msg) => console.error(...msg)
+    error: (...msg) => console.error(...msg),
 };
 
 module.exports = logger;
