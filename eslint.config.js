4fe1fb2bd5c37f29de15c204700f28cd6527f9e5
Eduardo David García Romero <eduagarcia985@gmail.com>
2025-11-17 21:48:59 -0500

    actualizacion 1.3
    regla de slint nueva

--- eslint.config.js
+++ eslint.config.js
@@ -11,6 +11,7 @@ module.exports = [
             ...js.configs.recommended.rules,
             semi: ['error', 'always'],
             quotes: ['error', 'single'],
+            'no-mixed-spaces-and-tabs': ['warn'],
             'no-unused-vars': ['warn'],
             'no-console': ['warn'],
             'eqeqeq': ['error', 'always'],
