3a569fc3de38ecc6196abf37ac03680ea1bb48a3
Eduardo David García Romero <eduagarcia985@gmail.com>
2026-01-15 19:40:02 -0500

    Actualizacion para prueba de carga y rendimiento
    funcion con token para que funcione

--- backend/carga-y-rendimiento.js
+++ backend/carga-y-rendimiento.js
@@ -6,16 +6,23 @@ export let options = {
         { duration: '10s', target: 10 },
         { duration: '30s', target: 50 },
         { duration: '10s', target: 0 },
-    ], thresholds: {
-        http_req_duration: ['p(95)<500'], 
-        http_req_failed: ['rate<0.01'] 
-    }
-}
+    ],
+    thresholds: {
+        http_req_duration: ['p(95)<500'],
+        http_req_failed: ['rate<0.01'],
+    },
+};
 
-//funcion principal del test
+const TOKEN = 'Pondran aqui el token que les de para que funcione la autenticacion';
+
 export default function () {
-    // Realizar una solicitud GET
-    const res = http.get('http://localhost:3000/api/players');
+    const params = {
+        headers: {
+            Authorization: `Bearer ${TOKEN}`,
+        },
+    };
+
+    const res = http.get('http://localhost:3000/api/players', params);
 
     check(res, {
         'status is 200': (r) => r.status === 200,
