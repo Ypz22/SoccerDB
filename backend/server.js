474ce731f76057529c326f3dd8b048b727673470
Teo344 <gabriel2004mat@gmail.com>
2026-02-07 19:17:11 -0500

    server y yml

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,6 @@ const app = require('./src/app');
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0',() => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
