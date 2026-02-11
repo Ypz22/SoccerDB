efc6e7936dfc897b450ff28d5afc42b945fd6eb3
Jefferson Yepez <jeffersonyepez621@gmail.com>
2026-02-11 13:22:44 -0500

    coverage tests

--- frontend/src/app/login/login.ts
+++ frontend/src/app/login/login.ts
@@ -32,7 +32,7 @@ export class LoginComponent {
       this.soccerService.login({ email: this.authData.email, password: this.authData.password })
         .subscribe({
           next: () => this.router.navigate(['/home']),
-          error: () => this.errorMessage = 'Credenciales inválidas'
+          error: (err) => this.errorMessage = 'Credenciales inválidas'
         });
     } else {
       this.soccerService.register(this.authData)
@@ -41,7 +41,7 @@ export class LoginComponent {
             this.isLoginMode = true;
             this.errorMessage = 'Registro exitoso. Por favor inicia sesión.';
           },
-          error: () => this.errorMessage = 'Error al registrar usuario'
+          error: (err) => this.errorMessage = 'Error al registrar usuario'
         });
     }
   }
